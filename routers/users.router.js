0555088dc923f1162b872ca844d8acaee49474e6
jeonsuwon <swjeon0330@gmail.com>
2024-05-24 17:08:05 +0900

    make login API

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,4 +1,6 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { prisma } from "../src/utils/prisma.util.js";
 
 const router = express.Router();
@@ -18,10 +20,12 @@ router.post("/sign-up", async (req, res, next) => {
       .json({ errorMessage: "이미 존재하는 이메일입니다." });
   }
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const user = await prisma.users.create({
     data: {
       email,
-      password,
+      password: hashedPassword,
     },
   });
 
@@ -36,6 +40,37 @@ router.post("/sign-up", async (req, res, next) => {
     },
   });
   return res.status(201).json({ message: "회원가입이 완료가 되었습니다." });
+});
+
+//사용자 로그인 API구현
+
+router.post("/sign-in", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.findFirst({
+    where: { email },
+  });
+  if (!user) {
+    return res
+      .status(401)
+      .json({ errorMessage: "등록되어있지 않는 이메일입니다." });
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res
+      .status(401)
+      .json({ errorMessage: "비밀번호가 일치하지 않습니다." });
+  }
+
+  //로그인에 성공한다면, 사용자에게 JWT를 발급합니다.
+  const token = jwt.sign(
+    {
+      userId: user.userId,
+    },
+    "customized_secret_key" // 비밀 키 , dotenv를 이용해서, 외부에서 코드를 보더라도, 알 수 없도록 구현해야함
+  );
+  res.cookie("authorization", `Bearer ${token}`);
+  return res.status(200).json({ message: "로그인 성공했습니다." });
 });
 
 export default router;
